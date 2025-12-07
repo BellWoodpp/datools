bb3df06d06d559409babf5dacb8a734965483baf
BellWoodpp <stormrobin50@gmail.com>
2025-12-07 23:37:36 +0800

    change light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,36 +39,8 @@ export default function RootLayout({
 }>) {
   return (
     // suppressHydrationWarning：抑制水和警告
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* 1. dangerouslySetInnerHTML：危险，设置内部HTML：加了它，脚本才能在 React 水和之前抢先执行 → 从第一帧开始就是正确的黑/白 → 丝滑无闪！
-            2. const theme = localStorage.getItem('theme'); 先去 localStorage 看看你上次选的是啥主题
-            3. if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark'); 如果你手动选过“dark”，或者你没选过但系统是暗黑模式 → 马上加 dark 类
-            4. else if (theme === 'light') {document.documentElement.classList.add('light') 
-                如果你手动选的是“light” → 加 light 类（有些项目会用到）;
-            5. else {const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-                document.documentElement.classList.add(systemTheme);} 其他情况（比如 localStorage 被清了）→ 按照你电脑/手机的系统设置来
-            6. catch (e) {} 万一 localStorage 被禁用（有些人开无痕模式），直接忽略错误}
-            */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              try {
-                const theme = localStorage.getItem('theme');
-                if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark');
-                } else if (theme === 'light') {
-                  document.documentElement.classList.add('light');
-                } else {
-                  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-                  document.documentElement.classList.add(systemTheme);
-                }
-              } catch (e) {}
-            `,
-          }}
-        />
-      </head>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
